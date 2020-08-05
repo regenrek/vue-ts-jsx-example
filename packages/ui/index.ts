90773297f7625320b3d46203931bf3c9911b85e9
Kevin Regenrek <hello@regenrek.at>
2020-08-05 21:50:45 +0200

    up

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,6 +2,6 @@ export const version = '0.0.95'
 
 export * from './NjBurger'
 export * from './NjButton'
-export * from './NjHeader'
 export * from './NjTabs'
 export * from './NjTextInput'
+export * from './NjThemeProvider'
--- typings/ui/NjThemeProvider/NjThemeProvider.d.ts
+++ typings/ui/NjThemeProvider/NjThemeProvider.d.ts
@@ -1,10 +1,15 @@
+export declare const anim: string;
 declare const _default: import("vue").ComponentOptions<import("vue-demi").Vue, {
+    themeVariant: string;
+} & {
     [key: string]: unknown;
 }, {}, {}, unknown, {
     [x: string]: any;
 }> & import("vue").VueConstructor<import("vue-demi").Vue> & (new (...args: any[]) => import("@vue/composition-api").ComponentRenderProxy<{
     [x: string]: any;
-}, unknown, {
+}, {
+    themeVariant: string;
+}, {
     [x: string]: any;
 }, {}, {}, {
     [x: string]: any;
